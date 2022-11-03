d75e776efc02d066c5a63fe086c26a540b3b9cef
Kavila Sushant <110624592+Sushant055@users.noreply.github.com>
2022-11-03 23:33:12 +0530

    Add files via upload

---
+++ HopeExists.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int w;
+    cin>>w;
+    if(w>=1 && w<=100)
+    {
+        if(w%2==1 || w==2)
+        cout<<"NO";
+        else cout<<"YES";
+    }
+    return 0;
+}
